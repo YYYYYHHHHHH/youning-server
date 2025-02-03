654a092cc4a4c34402f1cb788b19d727ad336802
YYYYYHHHHHH <1159161880@qq.com>
2025-02-03 03:48:55 +0000

    feat: 增加hook

---
+++ hooks/post-receive.sh
@@ -0,0 +1,11 @@
+   #!/bin/bash
+
+   # 读取标准输入的旧的和新的提交哈希值以及引用名称
+   while read oldrev newrev ref
+   do
+       # 检查是否是 master 分支
+       if [[ "$ref" == "refs/heads/master" ]]; then
+           # 触发 webhook
+           curl -X POST -H "Content-Type: application/json" -d '{"text": "Code pushed to master branch"}' http://192.168.11.102:3000/api/deploy/_8A2IuM245Ijc9a2Ge9Pr
+       fi
+   done
